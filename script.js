008ec9c4c1e6eab52ca1fdd2181bbc60d4c29779
painter98 <120320713+painter98@users.noreply.github.com>
2023-03-25 14:54:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 // complete the given function
-
 function palindrome(str){
-let start=0,end=str.length-1,ans;
+/*let start=0,end=str.length-1,ans;
 
 while(start<end){
     if(str.charAt(start++)!=str.charAt(end--)){
        return false;
     }
 }
-	return true;
+	return true;*/
+	var re = /[^A-Za-z0-9]/g; // or var re = /[\W_]/g;
+ str = str.toLowerCase().replace(re, '');
+
+ // Step 2. Create the FOR loop
+ var len = str.length; // var len = "A man, a plan, a canal. Panama".length = 30
+ 
+ for (var i = 0; i < len/2; i++) {
+   if (str[i] !== str[len - 1 - i]) { // As long as the characters from each part match, the FOR loop will go on
+       return false; // When the characters don't match anymore, false is returned and we exit the FOR loop
+   }
+	 }
+ return true; // Both parts are strictly equal, it returns true => The string is a palindrome
+
 }
 module.exports = palindrome
