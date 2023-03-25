faeb184814fe3e21884cafc2a374d1a4153e876f
painter98 <120320713+painter98@users.noreply.github.com>
2023-03-25 15:06:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // complete the given function
 function palindrome(str){
+/* //case sensitive 
 let start=0,end=str.length-1,ans;
-str=str.toLowerCase().replace(/[^A-Za-z0-9]/g,"");
-while(start<=end){
+
+while(start<end){
     if(str.charAt(start++)!=str.charAt(end--)){
        return false;
     }
 }
-	return true;
-	/*var re = /[^A-Za-z0-9]/g; // or var re = /[\W_]/g;
+	return true;*/
+	var re = /[^A-Za-z0-9]/g; // or var re = /[\W_]/g;
  str = str.toLowerCase().replace(re, '');
 
  // Step 2. Create the FOR loop
@@ -18,8 +19,5 @@ while(start<=end){
    if (str[i] !== str[len - 1 - i]) { // As long as the characters from each part match, the FOR loop will go on
        return false; // When the characters don't match anymore, false is returned and we exit the FOR loop
    }
-	 }
- return true; // Both parts are strictly equal, it returns true => The string is a palindrome
-*/
 }
 module.exports = palindrome
