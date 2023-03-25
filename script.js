7e7e7300bdf89b1820f89ea1493c393c2c6fa6c5
painter98 <120320713+painter98@users.noreply.github.com>
2023-03-25 14:42:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
+let start=0,end=str.length-1,ans;
 
+while(start<end){
+    if(str.charAt(start++)!=str.charAt(end--)){
+       return false;
+    }
+}
+	return true;
 }
 module.exports = palindrome
